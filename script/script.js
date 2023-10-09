b04eaf67b0807f7bcabc3be5221ffb9d69319ad3
CristianUrsino <ursino.cristian.uc@gmail.com>
2023-10-09 15:35:17 +0200

    added js basic algorithm

--- script/script.js
+++ script/script.js
@@ -4,3 +4,22 @@
 // va applicato uno sconto del 20% per i minorenni
 // va applicato uno sconto del 40% per gli over 65.
 // L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
+
+let testo = document.getElementById("testo");
+let km = parseInt(prompt('inserire km da percorrere: '));
+let eta = parseInt(prompt('inserire età: '));
+
+if(isNaN(km) || isNaN(eta) || km <=0 || eta < 0){
+    testo.innerHTML = "INSERIRE VALORE VALIDO (NUMERI INTERI POSITIVI)";
+    setTimeout(function() {
+        location.reload();
+    }, 2000);
+}
+
+let totale = km*0.21;
+console.log(totale);
+
+if(eta < 18) totale *= 0.8;
+else if(eta > 65) totale *= 0.6;
+
+testo.innerHTML = "PREZZO CALCOLATO: " + totale.toFixed(2) + "€";
